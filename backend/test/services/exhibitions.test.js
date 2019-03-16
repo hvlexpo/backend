3c6f692ac5d2a3ff04ff936285a3638356164a33
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-03-16 19:34:27 +0100

    :white_check_mark: Added test stubs

---
+++ backend/test/services/exhibitions.test.js
@@ -0,0 +1,13 @@
+const test = require('ava')
+
+if (process.env.NODE_ENV !== 'CI') {
+	require('dotenv').config()
+}
+
+const ExhibitionService = require('../../app/services/exhibition')
+
+test.todo('Create exhibition')
+test.todo('Read all exhibitions')
+test.todo('Read exhibition by id')
+test.todo('Update existing exhibition by id')
+test.todo('Delete exhibition by id')
