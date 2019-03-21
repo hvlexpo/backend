f3ca8708fd4c3b1b7970ab0e0e33733b47eea5b7
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-03-21 17:11:36 +0100

    :construction: Added check to see if POSTGRES_URL is present

--- backend/app/db/postgres.js
+++ backend/app/db/postgres.js
@@ -1,6 +1,13 @@
 const { Client } = require('pg')
 
-const postgres = new Client(process.env.POSTGRES_URL)
+const {POSTGRES_URL} = process.env
+
+if (!POSTGRES_URL) {
+	console.log('No Postgres URL. Exiting...')
+	process.exit(1)
+}
+
+const postgres = new Client(POSTGRES_URL)
 
 postgres.connect()
 
