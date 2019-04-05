f86deec5249a05bf0ac52048c731cde0e68b0643
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-04-05 13:30:45 +0200

    :sparkles: Updated votes

--- app/routes/votes.js
+++ app/routes/votes.js
@@ -24,15 +24,12 @@ router.get('/:exhibition_id/count', async (req, res) => {
 	res.send(await VotesService.read(req.user.id, req.params.exhibition_id))
 })
 
-// Create
-router.post('/', async (req, res) => {
-	res.send(await VotesService.create(req.user.id, req.body))
-})
-
 // Update
 router.put('/:exhibition_id', async (req, res) => {
+	const {weight} = req.body
+
 	res.send(
-		await VotesService.update(req.user.id, req.params.exhibition_id, req.body)
+		await VotesService.update(req.user.id, req.params.exhibition_id, weight)
 	)
 })
 
--- app/services/vote.js
+++ app/services/vote.js
@@ -1,19 +1,13 @@
 const postgres = require('../db/postgres')
 
 class VoteService {
-	static async create(userId, exhibitionId, {weight}) {
-		const value = [{ userId, exhibitionId, weight }]
+	static async create(user_id, exhibition_id, weight) {
+		const value = [{ user_id, exhibition_id, weight }]
 		const { rows } = await postgres.query(
 			'INSERT INTO votes(data) VALUES($1) RETURNING *',
 			value
 		)
 		return rows[0].data
-	}
-
-	static async readAll() {
-		const { rows } = await postgres.query('SELECT data FROM votes')
-		const response = rows.map(data => Object.values(data)[0])
-		return response
 	}
 
 	static async read(user_id, exhibition_id) {
@@ -25,8 +19,14 @@ class VoteService {
 		return rows[0].data
 	}
 
-	static async readForUser(userId) {
-		const value = [userId]
+	static async readAll() {
+		const { rows } = await postgres.query('SELECT data FROM votes')
+		const response = rows.map(data => Object.values(data)[0])
+		return response
+	}
+
+	static async readForUser(user_id) {
+		const value = [user_id]
 		const { rows } = await postgres.query(
 			"SELECT data FROM votes WHERE data ->> 'user_id' = $1",
 			value
@@ -35,7 +35,7 @@ class VoteService {
 		return rows.map(data => Object.values(data)[0])
 	}
 
-	static async update(user_id, exhibition_id, { weight }) {
+	static async update(user_id, exhibition_id, weight) {
 		const value = [{ user_id, exhibition_id, weight }, user_id, exhibition_id]
 		const { rows } = await postgres.query(
 			"UPDATE votes SET data = $1 WHERE data ->> 'user_id'= $2 AND data ->> 'exhibition_id' = $3 RETURNING *",
